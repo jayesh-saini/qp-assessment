575ca9aed966ab677f5f5c77719afff106523202
Jayesh Saini <sainijayesh815@gmail.com>
2024-03-30 14:23:25 +0530

    backend boiler-plate

---
+++ backend/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+
+const { PORT } = process.env
+
+app.use((req, res, next) => {
+    console.log('-------------------')
+    console.log(new Date().toString())
+    console.log(`${req.method} - ${req.path}`)
+    next()
+})
+
+// Routers
+
+
+// Health Check API
+app.get('/api/health', (req, res) => {
+    res.json({status: 200, message: 'Server is alive!'})
+})
+
+process.on('SIGINT', () => {
+    console.log('Stopping server')
+    process.exit()
+})
+
+process.on('uncaughtException', (errorCaught) => {
+    console.log('===========[Uncaught Exception Occured. Please check!]================\n')
+    console.log(errorCaught)
+    console.log('Stopping server')
+    process.exit()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Started on ${PORT}`);
+})
